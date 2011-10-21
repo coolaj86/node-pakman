4b5875184aed58d442ea8ba87afcc79b038b5db4
AJ ONeal <coolaj86@gmail.com>
2011-10-21 01:11:13 -0600

    npm just updated just now

--- test/test-compile.js
+++ test/test-compile.js
@@ -3,9 +3,7 @@
 
   require('Array.prototype.forEachAsync');
 
-  var npm = require('npm')
-    , fs = require('fs')
-    , compile = require('../lib/compile').compile
+  var compile = require('../lib/compile').compile
     , render = require('./example-template').render
     , moduleRoot = process.argv[2] || '../test_modules/foomodule'
     ;
